09dff006dd0f653ff4b13f82dc0da8f4f4013b89
abar44 <abarkhulci@gmail.com>
2025-08-21 11:36:04 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -47,7 +47,38 @@ module.exports.run = async function({ api, event }) {
 └───────────────⭓
  `,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+ }, event.threadID, () => fs.(__d 
+ │ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫 : 𝐌𝐚𝐥𝐞
+│ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 : 𝐒𝐢𝐧𝐠𝐥𝐞
+│ 🎂 𝐀𝐠𝐞 : 𝟏9+
+│ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧 : 𝐈𝐬𝐥𝐚𝐦
+│ 🎓 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : J𝐒𝐂 (𝟐𝟎𝟐4)
+│ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : DHAKA BANGLADESH 
+└───────────────⭓
+
+┌───────────────⭓
+│ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+├───────────────
+│ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
+│ https://www.facebook.com/share/1EwF5xTzM8/
+│ 💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
+│ https://wa.me/0162030345
+└───────────────⭓
+
+┌───────────────⭓
+│ 🕒 𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗧𝗶𝗺𝗲
+├───────────────
+│ ${time}
+└───────────────⭓
+ `,
+ attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
+
+ return request("https://i.imgur.com/idyXtoO.jpeg")
+ .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
+ .on('close', () => callback());
+};
+(__dirname + "/cache/owner.jpg"));
 
  return request("https://i.imgur.com/idyXtoO.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
